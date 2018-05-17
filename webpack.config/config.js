8079a1ef05bb965b6e6155a2fb6d7ec2885dd62b
Alan Chen <739709491@qq.com>
2018-05-17 19:49:12 +0800

    Update config.js

--- webpack.config/config.js
+++ webpack.config/config.js
@@ -1,6 +1,6 @@
 //此处设置devServer的端口和需要模块引入的第三方库,注意：如果想模块引入，必须先引入cdn！否则会报错
 module.exports = {
-    host: 'localhost'
+    host: 'localhost',
     port:8080,
     autoOpen: false,
     //并且需要设置别名和插件内挂载在windows对象上的别名
